84f5877a9db7296801df307cd8e23e0fa79a9f74
Michael Brown <michael.brown@socrata.com>
2014-11-07 10:37:56 -0800

    added a rbenv setup script

---
+++ setUpRbenv.sh
@@ -0,0 +1,7 @@
+cd ~
+git clone git://github.com/sstephenson/rbenv.git .rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+source ~/.bash_profile
+echo rbenv installed

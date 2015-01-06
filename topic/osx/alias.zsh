2c952f3dfb6095a37ae0a4e1ceb31dcfc2230216
Michael Brown <michael.brown@socrata.com>
2015-01-05 19:54:42 -0800

    finder needs to be lowercased

--- topic/osx/alias.zsh
+++ topic/osx/alias.zsh
@@ -1,5 +1,5 @@
 
 # Mac Helpers
-alias show_hidden="defaults write com.apple.Finder AppleShowAllFiles YES && killall Finder"
-alias hide_hidden="defaults write com.apple.Finder AppleShowAllFiles NO && killall Finder"
+alias show_hidden="defaults write com.apple.finder AppleShowAllFiles YES && killall Finder"
+alias hide_hidden="defaults write com.apple.finder AppleShowAllFiles NO && killall Finder"
 
